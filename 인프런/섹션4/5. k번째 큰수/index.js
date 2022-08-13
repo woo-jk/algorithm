1eb72b6811ca0b56d62a631488a9717d99927e41
Jungkyun Woo <73513965+woo-jk@users.noreply.github.com>
2022-08-13 21:25:11 +0900

    4-5 k번째 큰 수

---
+++ 인프런/섹션4/5. k번째 큰수/index.js
@@ -0,0 +1,34 @@
+function solution(n, k, card) {
+  let answer;
+  let sumThreeValue = [];
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      for (let k = j + 1; k < n; k++) {
+        sumThreeValue.push(arr[i] + arr[j] + arr[k]);
+      }
+    }
+  }
+  sumThreeValue.sort((a, b) => b - a);
+  sumThreeValue = [...new Set(sumThreeValue)];
+  answer = sumThreeValue[k - 1];
+  return answer;
+}
+
+// 답안
+function solution(n, k, card){
+  let answer;
+  let tmp = new Set();
+  for(let i=0; i<n; i++){
+      for(let j=i+1; j<n; j++){
+          for(let k=j+1; k<n; k++){
+              tmp.add(card[i]+card[j]+card[k]);
+          }
+      }
+  }
+  let a=Array.from(tmp).sort((a, b)=>b-a);
+  answer=a[k-1];
+  return answer;
+}
+
+let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
+console.log(solution(10, 3, arr));
