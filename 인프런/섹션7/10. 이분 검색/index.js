e5a527556f20494008261e5acb80965791f96303
Jungkyun Woo <73513965+woo-jk@users.noreply.github.com>
2022-08-18 17:46:49 +0900

    7-10

---
+++ 인프런/섹션7/10. 이분 검색/index.js
@@ -0,0 +1,39 @@
+function solution(target, arr) {
+  let answer;
+  arr.sort((a, b) => a - b);
+
+  while (arr.length > 1) {
+    console.log(arr.slice());
+    let med = Math.floor(arr.length / 2);
+    if (arr[med] === target) {
+      answer = med + 1;
+      break;
+    } else if (arr[med] < target) {
+      arr = arr.slice(med, arr.length);
+    } else {
+      arr = arr.slice(0, med);
+    }
+  }
+  return answer;
+}
+
+// 답안
+function solution(target, arr){
+  let answer;
+  arr.sort((a, b)=>a-b);
+  let lt=0, rt=arr.length-1;
+  while(lt<=rt){
+      let mid=parseInt((lt+rt)/2);
+      if(arr[mid]===target){
+          answer=mid+1;
+          break;
+      }
+      else if(arr[mid]>target) rt=mid-1;
+      else lt=mid+1;
+  }
+ 
+  return answer;
+}
+
+let arr = [23, 87, 65, 12, 57, 32, 99, 81];
+console.log(solution(32, arr));
