4a01a6158bc468530f2fb346ad0c63b0644ec410
Jungkyun Woo <73513965+woo-jk@users.noreply.github.com>
2022-08-23 09:25:14 +0900

    8-6, 8-7

---
+++ 인프런/섹션8/6. 바둑이 승차(!)/index.js
@@ -0,0 +1,36 @@
+// 내가 푼거
+// L이 arr크기보다 크면 sum+arr[L]의 값은 NaN이 뜸
+// -> 무한 재귀 발생 
+function solution(c, arr) {
+  let answer = 0;
+  function DFS(L, sum) {
+    if (sum + arr[L] > c) {
+      answer = Math.max(answer, sum);
+    } else {
+      DFS(L + 1, sum + arr[L]);
+      DFS(L + 1, sum);
+    }
+  }
+  DFS(0, 0);
+  return answer;
+}
+
+// 답안
+function solution(c, arr) {
+  let answer = Number.MIN_SAFE_INTEGER;
+  let n = arr.length;
+  function DFS(L, sum) {
+    if (sum > c) return;
+    if (L === n) {
+      answer = Math.max(answer, sum);
+    } else {
+      DFS(L + 1, sum + arr[L]);
+      DFS(L + 1, sum);
+    }
+  }
+  DFS(0, 0);
+  return answer;
+}
+
+let arr = [81, 58, 42, 33, 61];
+console.log(solution(259, arr));
