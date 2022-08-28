2047d37ada68378f6ca58e4a2bee6c8833eae1ac
Jungkyun Woo <73513965+woo-jk@users.noreply.github.com>
2022-08-29 01:04:17 +0900

    섹션10

---
+++ 인프런/섹션10/2. 돌다리 건너기/index.js
@@ -0,0 +1,26 @@
+function solution(n) {
+  let answer;
+  let dy = Array.from({ length: n + 1 }, () => 0);
+  dy[0] = 1;
+  dy[1] = 2;
+  for (let i = 2; i <= n + 1; i++) {
+    dy[i] = dy[i - 1] + dy[i - 2];
+  }
+  answer = dy[n];
+  return answer;
+}
+
+// 답안
+function solution(n){  
+  let answer=0;
+  let dy=Array.from({length:n+2}, ()=>0);
+  dy[1]=1;
+  dy[2]=2;
+  for(let i=3; i<=n+1; i++){
+      dy[i]=dy[i-2]+dy[i-1];
+  }
+  answer=dy[n+1];
+  return answer;
+}
+
+console.log(solution(7));
